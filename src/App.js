3b8c966276af67627d568a814385cb357f2b7e42
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-06-08 16:21:40 -0400

    - Create part three of robofriends app - Use state management between components

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+class App extends Component {
+  // adding state to our app
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: '',
+    };
+  }
+
+  // Search event
+  onSearchChange = (event) => {
+    // if you want to change the state of a value, you should use this.setState({the state : the value})
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+    return (
+      <div className='tc'>
+        <h1>Robofriends</h1>
+        <SearchBox searchChange={this.onSearchChange}></SearchBox>
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
+
+export default App;
