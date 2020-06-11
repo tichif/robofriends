74d9e1eb3bda4f06cac90a0ee02ad9c81493e033
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-06-10 21:50:22 -0400

    Create a scollable component in part 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -43,7 +44,9 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f1'>Robofriends</h1>
           <SearchBox searchChange={this.onSearchChange}></SearchBox>
-          <CardList robots={filteredRobots} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
         </div>
       );
     }
