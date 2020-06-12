238d280128b3ccd263d41fc1f435dfb45f884ab1
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-06-12 18:13:45 -0400

    - Make a redux async - Install redux-thumk - Combine multiple reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,13 +1,45 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_FAILED,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+} from './constants';
 
-const initialState = {
+const initialStateSearch = {
   searchField: '',
 };
 
-export const searchRobots = (state = initialState, action = {}) => {
+// reducer for search robots
+export const searchRobots = (state = initialStateSearch, action = {}) => {
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, { searchField: action.payload });
+    default:
+      return state;
+  }
+};
+
+const initialStateRequestRobots = {
+  isPending: false,
+  robots: [],
+  error: '',
+};
+
+// reducer for fetch robots
+export const fetchRobots = (state = initialStateRequestRobots, action = {}) => {
+  switch (action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return Object.assign({}, state, { isPending: true });
+    case REQUEST_ROBOTS_SUCCESS:
+      return Object.assign({}, state, {
+        robots: action.payload,
+        isPending: false,
+      });
+    case REQUEST_ROBOTS_FAILED:
+      return Object.assign({}, state, {
+        error: action.payload,
+        isPending: false,
+      });
     default:
       return state;
   }
